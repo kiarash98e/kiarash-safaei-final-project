c8874eda51b1ccb4b1ed34f4a9f0661aa0c36bf8
kiarashsafaei98 <kiarashsafaei0@gmail.com>
2021-08-02 18:33:09 +0430

    add main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Banner from './components/Banner/Banner'
 import Header from './components/Header/Header'
+import ProductCarouselSwiper from './components/ProductCarouserlSwiper/ProductCarouselSwiper'
+import TitleHomeSection from './components/TitleHomeSection/TitleHomeSection'
 
 function App() {
   return (
-    <div>
-      <Header/>      
+    <div style={{
+      height:'100%',
+      width:'100%'
+    }}>
+      <Header/>   
+      <Banner/>  
+      <TitleHomeSection title={'جدیدترین ها'}/> 
+      <ProductCarouselSwiper/>
     </div>
   )
 }
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container,Row,Col} from 'react-bootstrap'
+const Main = () => {
+    return (
+        <Container>
+            <Row>
+                <Col xs={12}>1</Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Main;
