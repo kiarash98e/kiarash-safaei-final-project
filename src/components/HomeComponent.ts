d61f9b5dcf8348ff84c5fc6ec0557417b46160e5
kiarashsafaei98 <kiarashsafaei0@gmail.com>
2021-08-03 12:21:33 +0430

    add and extract compnent for home page

---
+++ src/components/HomeComponent.ts
@@ -0,0 +1,10 @@
+import Banner from './Banner/Banner'
+import CatlogAds from './CatlogAds/CatlogAds'
+import Header from './Header/Header'
+import ProductCarouselSwiper from './ProductCarouserlSwiper/ProductCarouselSwiper'
+import TitleHomeSection from './TitleHomeSection/TitleHomeSection'
+import ShowOption from './ShowOption/ShowOption'
+import Footer from './Footer/Footer'
+
+
+export {Header,Footer,Banner,ShowOption,CatlogAds,TitleHomeSection,ProductCarouselSwiper}
