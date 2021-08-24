828759bb15d20a13fa1bed6e295c7bdbfb7acfe1
kiarashsafaei98 <kiarashsafaei0@gmail.com>
2021-08-24 16:29:18 +0430

    add productcart ui

--- src/components/ProductCart/ProductCart.tsx
+++ src/components/ProductCart/ProductCart.tsx
@@ -1,8 +1,21 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable array-callback-return */
+
 import React from 'react';
-import {OverlayTrigger,Tooltip} from 'react-bootstrap'
-import { FaShoppingCart } from 'react-icons/fa';
+import {CloseButton, OverlayTrigger,Tooltip} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import { FaShoppingCart } from 'react-icons/fa'
+import { FaMinus, FaPlus } from 'react-icons/fa'
+import {useSelector,useDispatch} from 'react-redux'
+import {increasItem,removeItem,cleanCart} from '../../Action/ItemAction'
+import './ProductCart.style.css'
+import TotalPrice from '../TotalPrice/TotalPrice';
+
 const ProductCart = () => {
+    const cart = useSelector((state:any) => state.products.cart)
+    console.log(cart.map((item:any) => item._id))
+    const dispatch = useDispatch()
+    const totalPrice = useSelector((state:any) => state.products.total)
+    console.log(totalPrice)
     return (
         <>
             <OverlayTrigger
@@ -10,24 +23,59 @@ const ProductCart = () => {
                 
                 placement='bottom'
                 overlay={
-                    <Tooltip id={`tooltip-${'button'}`}>
-                        <div className='d-flex p-2' style={{width:'12rem'}}>
-                            <div className="img-cart"></div>
-                            <div className="cart-body">
-                                <div className="title">پیراهن آبی</div>
-                                <div className="price">
-                                    <span>قیمت :</span>
-                                    <span>30.000 $</span>
+                    <Tooltip id={`tooltip-${'button'}`} className='tooltip mt-2 rounded-2'  style={{width:'20rem',margin:0,zIndex:99,backgroundColor:'#333',color:'#ffe8d6'}}>
+                    {
+                        cart.length > 0 ? (
+                            cart.map((cart:any) => (
+                                <div className='row' style={{height:'auto',width:'100%',padding:'10px',margin:0,backgroundColor:'#333'}}>
+                                    <div className='d-flex justify-content-between w-100 p-2 m-0' style={{width:"100%",backgroundColor:'transparent'}}>
+                                        <div className="img-cart">
+                                            <img src={cart.image} style={{width:'3rem'}} alt={cart.name} />
+                                        </div>
+                                        <div className="cart-info pt-1 pr-4" style={{width:'15rem'}}>
+                                            <div className="row p-0 m-0">
+                                                <div className="title col-12 text-light" style={{width:'12rem'}} >{cart.name}</div>
+                                                <div className="price col-12 pt-2" style={{fontSize:'.8rem',width:'10rem'}}>
+                                                    <span>قیمت :</span>
+                                                    <span className='ps-2' >{cart.price} تومان</span>
+                                                </div>
+                                                <div className="count pt-2 col-12 d-flex">
+                                                    <button className="btn btn-light text-dark" onClick={() => dispatch(increasItem(cart._id))}><FaPlus/></button>
+                                                    <span className='ps-1 pe-1 pt-2 align-middle'>{cart.quantity}</span>
+                                                    <button className="btn btn-light text-dark" onClick={() => dispatch(removeItem(cart._id))}><FaMinus/></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <CloseButton 
+                                            onClick={() => dispatch(cleanCart(cart._id))}
+                                            color='#f00'
+                                            variant='white'
+                                        />
+                                        
+                                        </div>
+                                    <div>
                                 </div>
                             </div>
-                            <button className="btn-delet-cart btn" style={{color:'#f00'}}>حذف</button>
-                        </div>
+                       
+                            ))
+                        ):
+                        <div style={{width:'10rem',textAlign:'center'}}>سبد خرید خالی است</div>
+                        
+                    }
+                    <div className="row" style={{
+                         height:'auto',
+                         width:'100%',
+                         padding:'10px',
+                         margin:0
+                     }}>
+                         <TotalPrice/>
+                     </div>   
                     </Tooltip>
                 }
-                delay={{show:300,hide:2000}}
-                >
-                <a href='#' style={{textDecoration:'none',color:'#E59C2C',display: 'inline-block',
-                }}><FaShoppingCart/></a>
+                delay={{show:300,hide:3500}}
+                >     
+                <Link to={'/cart'} style={{textDecoration:'none',color:'#ffe8d6',display: 'inline-block',
+                }}><FaShoppingCart/></Link>
                 </OverlayTrigger>
            
         </>
