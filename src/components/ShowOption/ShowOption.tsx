a5a3b5d6d74dbb4ab4b3865b6c28a39f0e2e6d62
kiarashsafaei98 <kiarashsafaei0@gmail.com>
2021-08-24 16:30:47 +0430

    add total price

--- src/components/ShowOption/ShowOption.tsx
+++ src/components/ShowOption/ShowOption.tsx
@@ -13,21 +13,21 @@ const ShowOption = () => {
         <Container>
             <Row className='pr-3 mt-3 py-5' style={ShowStyle}>
                
-                <Col xs={6} md={3} className='pt-3 d-flex justify-content-between'>
+                <Col xs={6} md={3} className='pt-3 d-flex flex-column align-items-center'>
                     <img src={ImgOption1} alt="" />
-                    <p className="mr-auto d-flex align-items-center">ضمانت اصل بودن کالا</p>
+                    <p className="mx-auto pt-4 align-items-center">ضمانت اصل بودن کالا</p>
                 </Col>
-                <Col xs={6} md={3} className='pt-3 d-flex justify-content-between'>
+                <Col xs={6} md={3} className='pt-3 d-flex flex-column align-items-center'>
                     <img src={ImgOption2} alt="" />
-                    <p className="mr-auto d-flex align-items-center">7 روز ضمانت کالا</p>
+                    <p className="mx-auto pt-4 d-flex align-items-center">7 روز ضمانت کالا</p>
                 </Col>
-                <Col xs={6} md={3} className='pt-3 d-flex justify-content-between'>
+                <Col xs={6} md={3} className='pt-3 d-flex flex-column align-items-center'>
                     <img src={ImgOption3} alt="" />
-                    <p className="mr-auto d-flex align-items-center">پرداخت درب محل</p>
+                    <p className="mx-auto pt-4 d-flex align-items-center">پرداخت درب محل</p>
                 </Col>
-                <Col xs={6} md={3} className='pt-3 d-flex justify-content-between'>
+                <Col xs={6} md={3} className='pt-3 d-flex flex-column align-items-center'>
                     <img src={ImgOption4} alt="" />
-                    <p className="mr-auto d-flex align-items-center">پشتیبانی 24 ساعته</p>
+                    <p className="mx-auto pt-4 d-flex align-items-center">پشتیبانی 24 ساعته</p>
                 </Col>
 
             </Row>
--- src/components/TitleHomeSection/TitleHomeSection.tsx
+++ src/components/TitleHomeSection/TitleHomeSection.tsx
@@ -1,54 +1,31 @@
 import { Container,Row,Col } from 'react-bootstrap'
 import { FaCube } from 'react-icons/fa'
-import SplitText from 'react-pose-text'
-import Aos from 'aos'
-import 'aos/dist/aos.css'
 import './TitleHomeSection.css'
 
 interface ITitle {
     title:string
 }
-interface WordAnim {
-    exit:{
-        opacity:number,
-        x:number | string
-    },
-    enter:{
-        opacity:number,
-        x:number | string
-        delay:(key:number) => void
-    }
-}
 
-const wordPoses:WordAnim = {
-    exit: { opacity: 0, x: 20 },
-    enter: {
-      opacity: 1,
-      x: 0,
-      delay: (wordIndex:number) => wordIndex * 100
-    }
-  }
+
+
 
 const cubeStyle = {
     fontSize:'1rem',
-    color:'#E59C2C',
     marginTop:'1rem',
     marginBottom:'3rem'
 }
 
 const TitleHomeSection: React.FC<ITitle> = ({title=""}) => {
-    Aos.init()
+   
     return (
         <Container>
             <Row>
                 <Col>
                     <div className="text-center">
                         <div className="row">
-                            <div className="col-12 pt-3">
-                               <h2 style={{color:'#333'}} className="title-home" data-aos='fade-left'>
-                                    <SplitText initialPose="exit" pose="enter" wordPoses={wordPoses}>
-                                        {title}
-                                    </SplitText>
+                            <div className="col-12 pt-3 text-dark">
+                               <h2 style={{color:'#333'}} className="title-home">
+                                    {title}
                                </h2>
                             </div>
                             <div className="col-12 cube-container">
