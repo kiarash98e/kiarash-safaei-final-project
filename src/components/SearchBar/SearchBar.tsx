afa1ebeba3a318d1ad3d8c2ad69e262483cc541b
kiarashsafaei98 <kiarashsafaei0@gmail.com>
2021-08-24 16:29:33 +0430

    modify searchbar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import {InputGroup,FormControl} from 'react-bootstrap'
 import {FaSearch} from 'react-icons/fa'
+import {useSelector,useDispatch} from 'react-redux'
+import {setTextFilter} from '../../Action/Filter.'
 import './Searchbar.style.css'
 const SearchBar = () => {
+    const inputValue = useSelector((state:any) => state.filters.name)
+    const dispatch = useDispatch()
+    const handleSearch = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        const value = e.target.value 
+        dispatch(setTextFilter(value))
+        
+    }
+    console.log(inputValue)
     return (
         <div>
             <InputGroup className="mb-3 box-search">
@@ -10,8 +20,10 @@ const SearchBar = () => {
                 className="search"
                 placeholder="جستو جو کنید"
                 aria-describedby="basic-search"
+                value={inputValue}
+                onChange={handleSearch}
                 />
-                <InputGroup.Text id="basic-search"><FaSearch/></InputGroup.Text>
+                <InputGroup.Text  id="basic-search"><FaSearch/></InputGroup.Text>
             </InputGroup>
         </div>
     );
