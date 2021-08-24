ae54139c52c4fbb7827fabb9254ea2756dade058
kiarashsafaei98 <kiarashsafaei0@gmail.com>
2021-08-24 16:26:13 +0430

    add show all product

---
+++ src/components/AllProduct/AllProduct.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-redeclare */
+import React from 'react'
+import Product from '../Product/Product'
+import {Col,Container,Row} from 'react-bootstrap'
+import SortPrice from '../SortPrice/SortPrice'
+
+const AllProduct:React.FC<any> = ({products}) => {
+    return (
+        <>
+           <div className="py-5">
+                <Container>
+                    <Row>
+                        <Col xs={12}>
+                            <h4 className='h-4'>فروشگاه</h4>
+                            <div className="my-3">
+                                <SortPrice/>
+                            </div>
+                            <div className="row">
+                                {
+                                    products && products.map((product: any)=>{
+                                        return(
+                                            <div className="col-12 col-md-6 col-xl-3 mx-auto pt-4"
+                                                key={product._id}
+
+                                            >
+                                                <Product product={product}/>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div> 
+        </>
+    )
+}
+
+export default AllProduct
