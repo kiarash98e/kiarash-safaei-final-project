9285ca195bfc15230878c39f3adae86a138518ed
kiarashsafaei98 <kiarashsafaei0@gmail.com>
2021-08-24 16:28:05 +0430

    modify navbar

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import LogoWeb from './logo-gold.png'
+import {Link} from 'react-router-dom'
 import './Logo.style.css'
 const Logo = () => {
     return (
-        <div>
-            <img src={LogoWeb} style={{marginTop:'-.75rem'}} alt="logo website" />
-        </div>
+        <Link to={'/'}>
+            <div>
+                <img src={LogoWeb} style={{marginTop:'-.75rem'}} alt="logo website" />
+            </div>
+        </Link>
     );
 }
 
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import {Container,Row,Col} from 'react-bootstrap'
+import {Banner,TitleHomeSection,ProductCarouselSwiper,ProductLikeSwiper,ProductNewSwiper,CatlogAds,ShowOption} from '../HomeComponent'
 const Main = () => {
     return (
         <Container>
             <Row>
-                <Col xs={12}>1</Col>
+                <Col xs={12}>
+                    <Banner/>
+                    <TitleHomeSection title={'جدیدترین ها'}/>
+                    <ProductCarouselSwiper/>
+                    <CatlogAds/>
+                    <TitleHomeSection title={'پرفروش ترین ها'}/>
+                    <ProductNewSwiper/>
+                    <TitleHomeSection title={'محبوب ترین ها'}/>
+                    <ProductLikeSwiper/>
+                    <ShowOption/>
+                </Col>
             </Row>
         </Container>
     );
